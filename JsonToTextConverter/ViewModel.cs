bc08f99ea3c24c3ece03a69d764ad301731bf60a
ajab1470 <ajab1470@gmail.com>
2026-03-11 22:07:40 -0400

    Add project files.

---
+++ JsonToTextConverter/ViewModel.cs
@@ -0,0 +1,89 @@
+﻿using System.ComponentModel;
+
+namespace JsonToTextConverter
+{
+    public class ViewModel() : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The relative or absolute path to the JSON file
+        /// </summary>
+        public string InputJson
+        {
+            get
+            {
+                return _inputJson;
+            }
+            set
+            {
+                _inputJson = value;
+                PropertyChangedEvent(nameof(InputJson));
+            }
+        }
+
+        // NEVER TOUCH THIS! use the property InputJson instead
+        private string _inputJson;
+
+        /// <summary>
+        /// The relative or absolute path to the desired output directory
+        /// </summary>
+        public string OutputDirectory
+        {
+            get
+            {
+                return _outputDirectory;
+            }
+            set
+            {
+                _outputDirectory = value;
+                PropertyChangedEvent(nameof(OutputDirectory));
+            }
+        }
+
+        // NEVER TOUCH THIS! use the property OutputDirectory instead 
+        private string _outputDirectory = string.Empty;
+
+        /// <summary>
+        /// If we should show the Hint tooltip
+        /// </summary>
+        public bool MoreThanOneUnsuccessfulButtonPress
+        {
+            get
+            {
+                return _unsuccessfulButtonPresses > 1;
+            }
+        }
+
+        private int _unsuccessfulButtonPresses = 0;
+
+        // This is for the view
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void Run()
+        {
+            // Input validation
+            if (!File.Exists(InputJson) || !"JSON".Equals(Path.GetExtension(InputJson), StringComparison.InvariantCultureIgnoreCase))
+            {
+                // this logic is for the gui
+                _unsuccessfulButtonPresses++;
+                PropertyChangedEvent(nameof(MoreThanOneUnsuccessfulButtonPress));
+
+                // if I cared more I'd make this better but I dont
+                return;
+            }
+
+            // !!JSON PARSING HERE!!
+
+            // this logic is for the view
+            _unsuccessfulButtonPresses = 0;
+        }
+
+        /// <summary>
+        /// Ignore me!
+        /// </summary>
+        /// <param name="propertyName">name of changed property</param>
+        protected void PropertyChangedEvent(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ SPETTApp/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using JsonToTextConverter;
+using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SPETTApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ViewModel _viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _viewModel = new ViewModel();
+            DataContext = _viewModel;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.Run();
+        }
+
+        private void InputFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+
+            // TODO FINISH
+        }
+
+        private void OutputFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFolderDialog();
+
+            // TODO FINISH
+        }
+    }
+}
